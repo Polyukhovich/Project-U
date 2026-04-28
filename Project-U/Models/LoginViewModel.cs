c499d76aff0cdd101d8701a21031305cff87aaea
Polyukhovich Andriy <kingg9940@gmail.com>
2026-04-28 23:15:31 +0300

    Додано AccountController, ViewModels, Views для Login та Register

---
+++ Project-U/Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectU.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Введіть email")]
+        [EmailAddress(ErrorMessage = "Невірний формат email")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Введіть пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        // Запам'ятати мене
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ Project-U/Models/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectU.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Введіть ім'я")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Введіть прізвище")]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Введіть email")]
+        [EmailAddress(ErrorMessage = "Невірний формат email")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Введіть пароль")]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Пароль мінімум 6 символів")]
+        public string Password { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Підтвердіть пароль")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Паролі не співпадають")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
