5bef02d9182e2ee319f33792639a360e3690835b
Polyukhovich Andriy <kingg9940@gmail.com>
2026-05-07 22:52:13 +0300

    Додано розклад на сьогодні на головній

--- Project-U/Controllers/HomeController.cs
+++ Project-U/Controllers/HomeController.cs
@@ -1,25 +1,46 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Localization;
+using ProjectU.Core.Models;
+using ProjectU.Data;
 
 namespace Project_U.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly IStringLocalizer<HomeController> _localizer;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(IStringLocalizer<HomeController> localizer)
+        public HomeController(IStringLocalizer<HomeController> localizer, ApplicationDbContext context)
         {
             _localizer = localizer;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            ViewBag.WelcomeMessage = _localizer["WelcomeMessage"];
-            return View();
-        }
+            var today = DateOnly.FromDateTime(DateTime.Today);
 
-        public IActionResult Privacy()
-        {
+            // Отримуємо поточного користувача
+            var currentUser = await _context.Users
+                .Include(u => u.Group)
+                .FirstOrDefaultAsync(u => u.UserName == User.Identity!.Name);
+
+            // Розклад на сьогодні для групи студента
+            var todaySchedules = await _context.Schedules
+                .Include(s => s.Course)
+                .Include(s => s.Group)
+                .Include(s => s.Dates)
+                .Where(s => s.GroupId == currentUser!.GroupId &&
+                            s.Dates.Any(d => d.Date == today))
+                .OrderBy(s => s.StartTime)
+                .ToListAsync();
+
+            ViewBag.TodaySchedules = todaySchedules;
+            ViewBag.Today = DateTime.Today.ToString("dddd, dd MMMM yyyy", new System.Globalization.CultureInfo("uk-UA"));
+
             return View();
         }
     }
